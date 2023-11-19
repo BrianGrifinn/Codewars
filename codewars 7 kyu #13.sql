c57672b2d35cdf2739acf256f691bb47380e290e
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-11-20 01:46:23 +0200

    Add files via upload

---
+++ codewars 7 kyu #13.sql
@@ -0,0 +1,12 @@
+/*Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+*/
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+ let i = laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  return i;
+}
