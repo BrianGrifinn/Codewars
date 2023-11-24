f6eca17f39b903da774ad4abe3ed2672dd0e4442
Brian Grifinn <126880335+BrianGrifinn@users.noreply.github.com>
2023-11-24 13:33:00 +0200

    Add files via upload

---
+++ codewars 7 kyu #21.sql
@@ -0,0 +1,12 @@
+/*
+You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+Complete the method which accepts such an array, and returns that single different number.
+
+The input array will always be valid! (odd-length >= 3)
+*/
+
+
+const stray = (numbers, sort = numbers.sort()) => 
+sort[0] == sort[1] ? sort[sort.length - 1] : sort[0]
+  
